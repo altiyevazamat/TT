65d4c5af20a1b23d99cf7997820843015e972d15
Azamat <altiyev.azamat@gmail.com>
2024-06-25 10:37:10 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strick";
+
+const answers = [];
+
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваше фамилия?", "");
+answers[2] = +prompt("Сколько вам лет?", "");
+
+console.log(
+  `Его зовут ${answers[0]}, а фамилия ${answers[1]}, ему ${answers[2]} лет`
+);
+
+let incr = 10,
+  dicr = 10;
+
+console.log(incr++);
+console.log(incr);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let incr = 10,
+  dicr = 10;
+
+console.log(incr++);
+console.log(incr);
