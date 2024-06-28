bbfe5edeeb9cd420b3d42ceb75dd58b3f1d6cfcb
Azamat <altiyev.azamat@gmail.com>
2024-06-28 21:38:20 +0500

    prac 4

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,10 +1,17 @@
-stroka = '';
-pust = ' ';
-for(let i=0;i<9;i++){
-    for(let j=9;j>9;j--){
-        stroka += '* \n' ; 
+function fib(num) {
+  let arr = [0, 1];
+  if (typeof num != 'number') {
+    return "";
+  } else if (num === 1) {
+    return '0'
+  }
+  else {
+    for (let i = 2; i < num; i++) {
+      arr[i] = arr[i - 2] + arr[i - 1];
     }
-    
-    
+  }
+
+  return arr.join(" ");
 }
-console.log(stroka)
+
+console.log(fib(1));
